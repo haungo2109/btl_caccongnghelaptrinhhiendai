3cac8bc0c517d39e9b2d212721a9c83c3867cc35
CosmicFishi <1851050025dung@ou.edu.vn>
2021-08-24 14:19:11 +0700

    added FE folder, added header

---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './header/header';
+
+function App() {
+  return (
+    <div className="main">
+      <div>
+        <Header />
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
