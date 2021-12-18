bec52224fbe466b3bdf584441581a9c0ef963f59
kaneki-sama <ngovanhau1999@gmail.com>
2021-12-18 09:48:26 +0700

    fix UI only, reomve small img when only one image, set fix height image, UI for mobile

--- front-end/src/components/shared/img-viewer.js
+++ front-end/src/components/shared/img-viewer.js
@@ -1,23 +1,32 @@
 import { useEffect } from 'react';
-import { useState } from 'react/cjs/react.development'
-import './img-viewer.css'
+import { useState } from 'react/cjs/react.development';
+import './img-viewer.css';
 
-export default function ImgViewer({imgArray, preview = true}) {
+export default function ImgViewer({ imgArray, preview = true }) {
+	const [mainImg, setMainImg] = useState(imgArray[0]);
 
-    const [mainImg, setMainImg] = useState(imgArray[0]);
+	useEffect(() => {
+		setMainImg(imgArray[0]);
+	}, [imgArray]);
 
-    useEffect(() => {
-        setMainImg(imgArray[0])
-    }, [imgArray])
-
-    return(
-        <div className="imgViewer-container">
-            <div className="first-preview">
-                {preview && <img src={mainImg} alt="Hình chính" />}
-            </div>
-            <div className="imgViewer-array">
-			    {imgArray && imgArray.map((c) => <img onClick={() => setMainImg(c)} key={c} src={c} alt="Hình" />)}
-            </div>
-        </div>
-    )
+	return (
+		<div className="imgViewer-container">
+			<div className="first-preview">
+				{preview && <img src={mainImg} alt="Hình chính" />}
+			</div>
+			{imgArray.length > 1 ? (
+				<div className="imgViewer-array">
+					{imgArray &&
+						imgArray.map((c) => (
+							<img
+								onClick={() => setMainImg(c)}
+								key={c}
+								src={c}
+								alt="Hình"
+							/>
+						))}
+				</div>
+			) : null}
+		</div>
+	);
 }
