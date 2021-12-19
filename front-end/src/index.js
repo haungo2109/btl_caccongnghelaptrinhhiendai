65035fc2db5180344bd64db027b5bc9241c0eb80
CosmicFishi <1851050025dung@ou.edu.vn>
2021-12-19 11:22:16 +0700

    added new dependencies, remember to do npm/yarn install

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { userReducer} from './userReducer';
+import * as serviceWorker from './service-worker';
 import reduxImutableStateInvariant from 'redux-immutable-state-invariant';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
@@ -22,6 +23,7 @@ const render = () => ReactDOM.render(
 
 render();
 store.subscribe(render);
+serviceWorker.register();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
